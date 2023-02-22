be8aeb83bb2bca4df785f7f7080635cd559226e9
julie thomas <juliethomas1008@gmail.com>
2023-02-22 14:20:36 +0100

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+8.times do
+  Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.street_address, phone_number: Faker::Number.leading_zero_number, category: ["chinese", "italian", "japanese", "french", "belgian"].sample)
+end
